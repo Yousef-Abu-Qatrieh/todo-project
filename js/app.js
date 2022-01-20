da359fd25c9e7cffe6c7422c71bd989381ebef6c
Yousef-Abu-Qatrieh <yousef_1995q@hotmail.com>
2022-01-20 12:13:50 +0200

    JS

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var name  = prompt("enter your name :")
+var gender =prompt("enter your gender :")
+var age = prompt ("enter your age ")
+if (gender == "male") {
+    alert("Welcome Mr "+ name )
+}else if (gender=="female"){
+    alert("Welcome Ms "+name)
+}
+else{
+    alert ("Welcome")
+
+}
+
+
+
+
+if (age>0) {
+  console.log(age)  
+
+}
+else
+{
+alert ("Invalid Input")
+}
